11fd0caa38c1c3f1a6b0f16468de8accf189c107
heynar-max <heynar76@hotmail.com>
2021-12-03 23:23:13 -0500

    commit1

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,78 @@
+import React, {useState} from 'react'
+import { Container,LogoSl, Menu, MenuItem, MenuItemLink, MobileIcon, Wrapper,} from './NavElements'
+import  logo  from '../assets/img/logosl.png'
+import Icon from './Icons'
+
+
+
+
+const Navbar = () => {
+    const [showMobileMenu, setshowMobileMenu] = useState(false)
+    return (
+        
+        <Container>
+            
+            <Wrapper>
+                
+            
+            <MobileIcon onClick={() => setshowMobileMenu(!showMobileMenu)}>
+                <Icon className="fas fa-bars"></Icon>
+            </MobileIcon>
+                <Menu open={showMobileMenu}>
+                    
+                        <MenuItem>
+                        
+                            <MenuItemLink >
+                            home
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink>
+                            about
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink>
+                            about
+                            </MenuItemLink>
+                        </MenuItem>
+                        
+                        
+                        <LogoSl>
+                        <img src={logo} width="150" height="150" alt="" />
+                        </LogoSl>
+                        
+                        
+                        <MenuItem>
+                            <MenuItemLink>
+                            oleee
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink>
+                            oleee
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink>
+                            mirame
+                            </MenuItemLink>
+                            
+                        </MenuItem>
+                        
+                        
+                    </Menu>
+                    
+            </Wrapper>
+            
+        </Container>
+        
+    )
+}
+
+
+
+
+
+export default Navbar
+
