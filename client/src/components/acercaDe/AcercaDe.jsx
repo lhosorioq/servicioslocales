1c2d86e201a5a95f3251ddcaec679345e5119cf9
Marcela <ardilamarcela42@gmail.com>
2021-12-06 13:16:22 -0500

    Acercade

--- client/src/components/acercaDe/AcercaDe.jsx
+++ client/src/components/acercaDe/AcercaDe.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import { MainLayout } from '../../assets/Styles/Layouts'
 import styled from 'styled-components'
 import Title from '../Titulos/Title'
 import TarjetasMision from './TarjetasMision'
+
 
 const AcercaDe = () => {
     return (
@@ -12,16 +13,48 @@ const AcercaDe = () => {
             <EquipoStyled >
                 <Title title={'Acerca De'} span={'Acerca De'} />
                 <div className="equipos">
-                    <TarjetasMision 
-                        title={'MISION'} 
-                        paragraph={'Somos un grupo de emprendedores que por medio del conocimiento en Programacion, llevamos las ideas en proyectos.Empezamos una carrera por lineas separadas, que con el paso del tiempo se fueron uniendo hasta formar un gran  Tecnologo en Electronica, Tecnologo en Alimentos, Bachillere e ingenieros de Sistemas, nos unimos formando un excelente grupo interdisciplinario aportando los mejores conocimientos en cada area de experiencia y llevandolo al mundo del desarrollo, para la creacion de empresa. '}
-                        
-                    />
-                    <TarjetasMision 
-                        title={'VISION'} 
-                        paragraph={'Apasionada al Desarrollo Frontend'}
-                        
-                    />
+                        <Row>
+                            <Col md={8} >
+                            <TarjetasMision 
+                            title={'MISION'} 
+                            paragraph={'Somos un grupo de emprendedores que por medio del conocimiento en Programacion, llevamos las ideas en proyectos.Empezamos una carrera por lineas separadas, que con el paso del tiempo se fueron uniendo hasta formar un gran  Tecnologo en Electronica, Tecnologo en Alimentos, Bachillere e ingenieros de Sistemas, nos unimos formando un excelente grupo interdisciplinario aportando los mejores conocimientos en cada area de experiencia y llevandolo al mundo del desarrollo, para la creacion de empresa. '}
+                            
+                        />
+                            
+                            </Col>
+                            
+                        </Row>
+
+                        <Row>
+                            <Col md={8} >
+                            <TarjetasMision 
+                            title={'VISION'} 
+                            paragraph={'Seguiremos brindando una mejor cooperación entre nuestros proveedores y usuario mediante la innovación, manteniendo nuestra eficiencia y excelencia, siempre teniendo en cuenta las necesidades de nuestros clientes.'}
+                            
+                        />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md={8} >
+                            <TarjetasMision 
+                            title={'hISTORIA'} 
+                            paragraph={'Empezamos una carrera por lineas separadas, que con el paso del tiempo se fueron uniendo hasta formar un gran G5. Tecnologo en Electronica, Tecnologo en Alimentos, Bachillere e ingenieros de Sistemas, nos unimos formando un excelente grupo interdisciplinario aportando los mejores conocimientos en cada area de experiencia y llevandolo al mundo del desarrollo, para la creacion de empresa. '}
+                            
+                        />
+                            </Col>
+
+                            
+                        </Row>
+                        <Row>
+                            <Col md={8} >
+                            <TarjetasMision 
+                            title={'QUIENES SOMOS'} 
+                            paragraph={'Somos un grupo de Emprendedores que por medio del conocimiento en desarrollo Web logramos plasmar una idea personal en un gran proyecto, para el beneficio de todas las comunidades en el país. '}
+                            
+                        />
+                            </Col>
+                            
+                        </Row>
                     
                 </div>
             </EquipoStyled>
@@ -50,6 +83,7 @@ const EquipoStyled = styled.section`
         }
         
     }
+
 `;
 
 export default AcercaDe
